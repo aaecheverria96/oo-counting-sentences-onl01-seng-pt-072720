cd5b30fdf448b889f56c4d37bc03e662a6c22c31
Andrew Echeverria <andrew.a.echeverria@gmail.com>
2020-08-21 04:40:43 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,31 @@ require 'pry'
 class String
 
   def sentence?
-    
+  if self.end_with?(".","?")
+    return true
+  else
+  return false
   end
+end
 
   def question?
-
+if self.end_with?("?")
+return true
+else
+  return false
+end
   end
 
   def exclamation?
-
+if self.end_with?("!")
+  return true
+else
+  return false
+end
   end
 
   def count_sentences
-
+    split_string = split_string = self.split(/\.|\?|\!/)
+    return split_string.count{|string| string != ""}
   end
 end
